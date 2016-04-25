c6806d93a8c9cc70012cd091941ebdcf4d212282
Vignesh Jayabalan <vigneshjayabalan@gmail.com>
2016-04-25 14:42:19 -0700

    printing the passed string if the number of args = 2

--- safeLib.c
+++ safeLib.c
@@ -2,13 +2,26 @@
 
 #include <stdio.h>
 #include <dlfcn.h>
+#include <stdarg.h>
 
 static int (*orig_printf)(const char *format, ...) = NULL;
 
 int printf(const char *format1, ...)
 {
-
+	int numArgs = 2, i=0;
+	va_list args;
 	orig_printf = (int (*)(const char *format, ...))dlsym(RTLD_NEXT, "printf");
-	int a =1;
-	return (*orig_printf)("%s\n", format1);
+
+	(*orig_printf)("This is a safe string \n");
+
+	va_start(args, numArgs);
+	char *format_str = va_arg(args,char *);
+	char *arg_item = va_arg(args,char *);
+	va_end(args);
+
+	(*orig_printf)(format_str, arg_item);
+
+	(*orig_printf)("\n\n\n---\n");
+
+	return 1;
 }
