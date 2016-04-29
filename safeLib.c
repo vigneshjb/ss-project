bd0aca3e9916ea12ea55c3dcdb5fe5acace91d2c
Vignesh Jayabalan <vigneshjayabalan@gmail.com>
2016-04-29 13:57:28 -0700

    all functions supported

--- safeLib.c
+++ safeLib.c
@@ -6,7 +6,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-static int (*orig_printf)(const char *format, ...) = NULL;
+static int (*orig_stream_print)(FILE *stream, const char *format, ...) = NULL;
+static int (*orig_string_print)(char *string, const char *format, ...) = NULL;
 
 int nonNformat(char a){
 	int res = 1;
@@ -33,20 +34,16 @@ int nonNformat(char a){
 	return res;
 }
 
-int printf(const char *format_str, ...)
-{
+void sanitized_stream_print(FILE *stream, const char* format_str, ...){
 	int numArgs, i=0, testflg=0;
-
 	char *our_format_str;
+	va_list args;
+
 	our_format_str = malloc(strlen(format_str));
 	strcpy(our_format_str, format_str);
-
-	va_list args;
 	
-	orig_printf = (int (*)(const char *format, ...))dlsym(RTLD_NEXT, "vprintf");
+	orig_stream_print = (int (*)(FILE *stream, const char *format, ...))dlsym(RTLD_NEXT, "vfprintf");
 
-	// (*orig_printf)("\n---\n");
-	puts(format_str);
 	while(1){
 		if (format_str[i]=='\0'|| i>strlen(format_str))
 			break;
@@ -64,14 +61,80 @@ int printf(const char *format_str, ...)
 		}
 		i++;
 	}
-	puts(our_format_str);
 
 	va_start(args, format_str);
-		(*orig_printf)(our_format_str, args);
+		(*orig_stream_print)(stream, our_format_str, args);
 	va_end(args);
-	// (*orig_printf)("\n---\n");
+}
+
+void sanitized_string_print(char *string, const char* format_str, ...){
+	int numArgs, i=0, testflg=0;
+	char *our_format_str;
+	va_list args;
+
+	our_format_str = malloc(strlen(format_str));
+	strcpy(our_format_str, format_str);
 	
+	orig_string_print = (int (*)(char *string, const char *format, ...))dlsym(RTLD_NEXT, "vsprintf");
 
+	while(1){
+		if (format_str[i]=='\0'|| i>strlen(format_str))
+			break;
+		else if(format_str[i]=='%' && format_str[i+1]=='%')
+			i++;
+		else if (format_str[i]=='%'){
+			testflg=1;
+		}
+		if (testflg==1 && nonNformat(format_str[i])==1){
+			testflg=0;
+		}
+		else if(testflg==1 && format_str[i]=='n'){
+			testflg=0; 
+			*(our_format_str+i)='p';
+		}
+		i++;
+	}
+
+	va_start(args, format_str);
+		(*orig_string_print)(string, our_format_str, args);
+	va_end(args);
+}
+
+int printf(const char *format, ...){
+	va_list args;
+	va_start(args, format);
+		sanitized_stream_print(stdout, format, args);
+	va_end(args);
 	return 1;
+}
 
+int fprintf(FILE *stream, const char *format, ...){
+	va_list args;
+	va_start(args, format);
+		sanitized_stream_print(stream, format, args);
+	va_end(args);
+	return 1;
+}
+
+int vprintf(const char *format, va_list ap){
+	sanitized_stream_print(stdout, format, ap);
+	return 1;
+}
+
+int vfprintf(FILE *stream, const char *format, va_list ap){
+	sanitized_stream_print(stream, format, ap);
+	return 1;
+}
+
+int sprintf(char *str, const char *format, ...){
+	va_list args;
+	va_start(args, format);
+		sanitized_string_print(str, format, args);
+	va_end(args);
+	return 1;
+}
+
+int vsprintf(char *str, const char *format, va_list ap){
+	sanitized_string_print(str, format, ap);
+	return 1;
 }
